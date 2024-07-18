596097a32fdf33cc07eac7b80cc199d689faff96
marceloreggiani <mreggiani@gmail.com>
2024-07-18 16:43:01 -0300

    commit da home e do contexts

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,51 @@
+import { useEffect, useState } from "react";
+import { Container, Card, Button } from "react-bootstrap";
+import { Link, Navigate } from "react-router-dom";
+// import { UsuarioContext } from "../contexts/UsuarioContext";
+// import { getListasUsuario } from "../firebase/listas"; // Assumindo que a função foi criada
+
 function Home() {
-    return(
-        <main>
-            <h1>Home</h1>
-        </main>
-    )
+  const [listas, setListas] = useState([]);
+//   const usuario = useContext(UsuarioContext);
+
+  useEffect(() => {
+    // if (usuario) {
+    //   getListasUsuario(usuario.uid).then((resultados) => {
+    //     setListas(resultados);
+    //   });
+    // }
+  }, []);
+
+  // if (usuario === null) {
+  //   return <Navigate to="/login" />;
+  // }
+
+  return (
+    <main className="mt-3" style={{ paddingTop: "16px", paddingBottom: "56px" }}>
+        <img src="/public/banner-listaexpress.png" className="img-fluid" alt="Banner Lista Express" />
+      <Container>
+        <h1>Suas Listas de Compras</h1>
+        <hr />
+        <Link className="btn btn-dark" to="/listas/adicionar">
+          Adicionar Lista
+        </Link>
+        <section className="mt-2">
+          {listas.map((lista) => (
+            <Card key={lista.id} className="mb-2">
+              <Card.Body>
+                <Card.Title>{lista.titulo}</Card.Title>
+                <Card.Text>{lista.descricao}</Card.Text>
+                <Button variant="dark" as={Link} to={`/listas/${lista.id}`}>
+                  Ver Itens
+                </Button>
+              </Card.Body>
+            </Card>
+          ))}
+        </section>
+      </Container>
+    </main>
+  );
 }
 
 export default Home;
+
