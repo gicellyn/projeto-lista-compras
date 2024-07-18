9633ac787959d1042247d0bf3241ceb395223737
gicellyn <gicellynayara@gmail.com>
2024-07-18 15:20:54 -0300

    adição  component Loader

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,20 +5,20 @@ function Menu() {
     return (
         <header>
             <Navbar bg="light" variant="light" expand="md">
-        <Container fluid>
-          <Link to="/">
-            <img src="/logo-lista.png" width="150" alt="Logo" />
-          </Link>
-        </Container>
-        <Container>
-            <Navbar.Toggle />
-             <Navbar.Collapse>
-                <Nav className="ms-auto">
-                    <Link className="nav-link" to="/">Home</Link>
-                     <Link className="nav-link" to="/login">Login</Link>
-                    <Link className="nav-link" to="/cadastro">Cadastro</Link>
-                    <Link className="nav-link" to="/ajuda">Ajuda</Link>
-                </Nav>
+                <Container fluid>
+                    <Link to="/">
+                        <img src="/logo-lista.png" width="150" alt="Logo" />
+                    </Link>
+                </Container>
+                <Container>
+                    <Navbar.Toggle />
+                    <Navbar.Collapse>
+                        <Nav className="ms-auto">
+                            <Link className="nav-link" to="/">Home</Link>
+                            <Link className="nav-link" to="/login">Login</Link>
+                            <Link className="nav-link" to="/cadastro">Cadastro</Link>
+                            <Link className="nav-link" to="/ajuda">Ajuda</Link>
+                        </Nav>
                     </Navbar.Collapse>
                 </Container>
             </Navbar>
