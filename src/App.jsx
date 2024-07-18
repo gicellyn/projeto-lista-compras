5706a7ac54371507eb27d7faf8a34dc11fba4d23
Carol <carolcavalcanty@gmail.com>
2024-07-18 13:59:45 -0300

    navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,32 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Cadastro from "./pages/Cadastro";
+import Ajuda from "./pages/Ajuda";
+import NotFound from "./pages/NotFound";
+import NovaTarefa from "./pages/NovaTarefa";
+
+// BrowserRouter: componente essencial para conduzir o roteamento no navegador.
+// Route: indicamos a rota (path) e o elemento que será exibido na tela.
+
 function App() {
-  
   return (
     <>
+      <BrowserRouter>
+        <Navbar/>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/cadastro" element={<Cadastro />} />
+          <Route path="/ajuda" element={<Ajuda />} />
+          <Route path="/novatarefa" element={<NovaTarefa />}/>
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
       
     </>
-  )
+  );
 }
 
 export default App;
