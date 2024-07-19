61bc3a7f80729c417836328df4eceeb6888929ee
gicellyn <gicellynayara@gmail.com>
2024-07-19 16:29:35 -0300

    atualização navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -29,10 +29,10 @@ function Menu() {
                     <Navbar.Collapse>
                         <Nav className="ms-auto">
                            {user && <Link className="nav-link" to="/">Home</Link>}
-                           {!user && <Link className="nav-link" to="/listas">Listas</Link>}
+                           {user && <Link className="nav-link" to="/listas">Listas</Link>}
                             {!user && <Link className="nav-link" to="/login">Login</Link>}
-                           {user && <Link className="nav-link" to="/cadastro">Cadastro</Link>}
-                           {user && <Button variant="outline-light" onClick={handleLogout}></Button>}
+                           {!user && <Link className="nav-link" to="/cadastro">Cadastro</Link>}
+                           {user && <Button variant="danger" onClick={handleLogout}>Sair</Button>}
                         </Nav>
                     </Navbar.Collapse>
                 </Container>
